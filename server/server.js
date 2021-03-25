2908a67f66924221b201ece2b82778ba90f92ed6
fibanneacci <24998524+fibanneacci@users.noreply.github.com>
2021-03-24 23:54:28 -0500

    remove unused

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 require('dotenv').config({ path: '../.env' });
 const { TwitterClient } = require('twitter-api-client');
-//const Twitter = require('twitter');
 const express = require("express");
 
 const twitterClient = new TwitterClient({
@@ -9,12 +8,6 @@ const twitterClient = new TwitterClient({
   accessToken: process.env['TWITTER_ACCESS_TOKEN'],
   accessTokenSecret: process.env['TWITTER_ACCESS_TOKEN_SECRET']
 });
-// var client = new Twitter({
-//   consumer_key: process.env['TWITTER_API_KEY'],
-//   consumer_secret: process.env['TWITTER_API_KEY_SECRET'],
-//   access_token_key: process.env['TWITTER_ACCESS_TOKEN'],
-//   access_token_secret: process.env['TWITTER_ACCESS_TOKEN_SECRET']
-// });
 
 async function get_friends() {
   const data = await twitterClient.accountsAndUsers.friendsList();
@@ -30,27 +23,6 @@ async function get_home_timeline() {
 }
 
 get_home_timeline()
-
-// function get_home_timeline() {
-//   client.stream('statuses/home_timeline', {}, function (stream) {
-//     stream.on('data', function (tweet) {
-//       console.log(tweet.text);
-//     });
-
-//     stream.on('error', function (error) {
-//       console.log(error);
-//     });
-//   });
-// }
-
-// get_home_timeline()
-
-// var params = {screen_name: '_anli'};
-// client.get('statuses/user_timeline', params, function(error, tweets, response) {
-//   if (!error) {
-//     console.log(tweets);
-//   }
-// });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
