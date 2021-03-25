78316732223d2c75d7305ac33c95a503af3711b0
fibanneacci <24998524+fibanneacci@users.noreply.github.com>
2021-03-25 01:31:07 -0500

    cursor half working

--- server/server.js
+++ server/server.js
@@ -10,19 +10,55 @@ const twitterClient = new TwitterClient({
 });
 
 async function get_friends() {
-  const data = await twitterClient.accountsAndUsers.friendsList();
-  console.log(data)
+  var prev_cursor = 0;
+  var cursor = -1;
+  var params = { };
+  while (prev_cursor != cursor) {
+    const data = await twitterClient.accountsAndUsers.friendsList(params);
+    //console.log(data)
+    console.log(data.users.length)
+    for (let i = 0; i < data.users.length; i++) {
+      console.log(data.users[i].profile_image_url_https)
+    }
+    prev_cursor = cursor;
+    cursor = data.next_cursor;
+    params.cursor = cursor;
+  }
 }
 
-//get_friends()
+get_friends()
+
+class Tweet {
+  constructor(time, id, text, name, profile_picture, likes, liked) {
+    this.time = time;
+    this.id = id;
+    this.text = text;
+    this.name = name;
+    this.profile_picture = profile_picture;
+    this.likes = likes;
+    this.liked = liked;
+  }
+}
+
+var home_timeline_tweets_set = new Set()
+var home_timeline_tweets = new Array() // Haven't yet decided how to do this — existing Tweets need to be updated if likes / liked changed
 
 async function get_home_timeline() {
-  var params = { count: 20, exclude_replies: true }
+  var params = { count: 20, exclude_replies: true };
   const data = await twitterClient.tweets.statusesHomeTimeline(params);
-  console.log(data)
+  for (let i = data.length - 1; i >= 0; i--) {
+    console.log(data[i].created_at)
+    console.log(data[i].id)
+    console.log(data[i].text)
+    console.log(data[i].user.screen_name)
+    console.log(data[i].user.profile_image_url_https)
+    console.log(data[i].favorite_count)
+    console.log(data[i].favorited)
+    console.log("\n")
+  }
 }
 
-get_home_timeline()
+//get_home_timeline()
 
 const app = express();
 const PORT = process.env.PORT || 3001;
