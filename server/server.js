d59077bebbdb8c4090d0c324c0e2d90f40565f7a
fibanneacci <24998524+fibanneacci@users.noreply.github.com>
2021-03-25 15:01:28 -0500

    done w/ twitterclient calls

--- server/server.js
+++ server/server.js
@@ -11,7 +11,13 @@ const twitterClient = new TwitterClient({
   accessTokenSecret: process.env['TWITTER_ACCESS_TOKEN_SECRET']
 });
 
-// Class storing essential elements from Tweets
+class Friend {
+  constructor(name, profile_picture) {
+    this.name = name;
+    this.profile_picture = profile_picture;
+  }
+}
+
 class Tweet {
   constructor(time, id, text, name, profile_picture, likes, liked) {
     this.time = time;
@@ -24,32 +30,39 @@ class Tweet {
   }
 }
 
-// Latest home timeline of Tweets, updated cumulatively with each call to get_home_timeline
+var friends = new Array()
 var home_timeline = new Array()
+
+// Print current-following users (for debugging)
+async function print_friends() {
+  for (let i = 0; i < friends.length; i++) {
+    console.log(friends[i]);
+  }
+  console.log("\n");
+}
 
 // Get currently-following users
 async function get_friends() {
   try {
-    var prev_cursor = 0;
-    var cursor = -1;
-    var params = {};
-    while (prev_cursor != cursor) {
+    var params = { cursor: -1 };
+    while (params.cursor != 0) {
       const data = await twitterClient.accountsAndUsers.friendsList(params);
       console.log(data.users.length);
       for (let i = 0; i < data.users.length; i++) {
-        console.log(data.users[i].profile_image_url_https);
+        var cur_friend = new Friend(data.users[i].screen_name, data.users[i].profile_image_url_https);
+        friends.push(cur_friend);
       }
-      prev_cursor = cursor;
-      cursor = data.next_cursor;
-      params.cursor = cursor;
+      params.cursor = data.next_cursor;
     }
+    print_friends();
   } catch (e) {
     console.log("Error: ", e);
   }
 }
 
-//get_friends();
+get_friends();
 
+// Print Tweets in home timeline (for debugging)
 async function print_home_timeline() {
   for (let i = 0; i < home_timeline.length; i++) {
     console.log(home_timeline[i]);
@@ -74,14 +87,8 @@ async function get_home_timeline() {
     var params = { count: HOME_TIMELINE_COUNT, exclude_replies: true };
     const data = await twitterClient.tweets.statusesHomeTimeline(params);
     for (let i = data.length - 1; i >= 0; i--) {
-      var cur_tweet = new Tweet();
-      cur_tweet.time = data[i].created_at;
-      cur_tweet.id = data[i].id;
-      cur_tweet.text = data[i].text;
-      cur_tweet.name = data[i].user.screen_name;
-      cur_tweet.profile_picture = data[i].user.profile_image_url_https;
-      cur_tweet.likes = data[i].favorite_count;
-      cur_tweet.liked = data[i].favorited;
+      var cur_tweet = new Tweet(data[i].created_at, data[i].id, data[i].text, data[i].user.screen_name,
+        data[i].user.profile_image_url_https, data[i].favorite_count, data[i].favorited);
       if (!update_Tweet(cur_tweet)) {
         home_timeline.push(cur_tweet);
       }
