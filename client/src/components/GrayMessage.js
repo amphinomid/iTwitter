6ba521cb8bb83abf7745a42e2611a497e4bbce23
fibanneacci <24998524+fibanneacci@users.noreply.github.com>
2021-03-26 15:15:45 -0500

    finish gray messages

--- client/src/components/GrayMessage.js
+++ client/src/components/GrayMessage.js
@@ -1,12 +1,16 @@
 import './Message.css'
 import TweetProfilePicture from './TweetProfilePicture'
+import TailLeft from '../assets/tail_left.png'
 
 function GrayMessage(props) {
     return (
         <div className="gray-tweet">
             <TweetProfilePicture name={props.name} url={props.profile_picture} diameter={40} />
             <div className="gray-message">
-                <p className="tweet-text">{props.text}</p>
+                <div className="gray-bubble">
+                    <p className="tweet-text">{props.text}</p>
+                </div>
+                <img className="gray-tail" src={TailLeft} alt='Tail of gray message bubble' />
             </div>
         </div>
     )
