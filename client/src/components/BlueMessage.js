82cf8af44d9e966167dc42f92d9708259e38be83
fibanneacci <24998524+fibanneacci@users.noreply.github.com>
2021-03-28 16:09:53 -0500

    own likes icons working

--- client/src/components/BlueMessage.js
+++ client/src/components/BlueMessage.js
@@ -6,6 +6,22 @@ import LikeLeftBlue from '../assets/like_left_blue_border.png'
 let textTolink = require('text-to-link')
 
 class BlueMessage extends React.Component {
+    addLike(id) {
+        var first_like = document.getElementById(`first-like-${id}`);
+        var second_like = document.getElementById(`second-like-${id}`);
+        var third_like = document.getElementById(`third-like-${id}`);
+
+        if (third_like && third_like.style.visibility === 'visible') {
+            // Third blue
+        } else if (second_like && second_like.style.visibility === 'visible') {
+            // Third blue
+        } else if (first_like && first_like.style.visibility === 'visible') {
+            // Second blue
+        } else {
+            // First blue
+        }
+    }
+
     componentDidMount() {
         var tweet_texts = document.getElementsByClassName("blue-tweet-text");
         for (let i = 0; i < tweet_texts.length; i++) {
@@ -17,33 +33,21 @@ class BlueMessage extends React.Component {
             var bubble = document.getElementById(bubble_id);
             likes[i].style.marginBottom = `${bubble.clientHeight - 16}px`;
             likes[i].style.marginRight = `${bubble.clientWidth + 7}px`;
-
-            var first_like = document.getElementById(`first-like-${this.props.id}`);
-            var second_like = document.getElementById(`second-like-${this.props.id}`);
-            var third_like = document.getElementById(`third-like-${this.props.id}`);
-
-            if (third_like.style.visibility == 'visible') {
-
-            } else if (second_like.style.visibility == 'visible') {
-
-            } else if (first_like.style.visibility == 'visible') {
-                
-            }
         }
     }
 
     render() {
         return (
             <div className="blue-tweet">
-                <div className="blue-message">
+                <div className="blue-message" onDoubleClick={() => {this.addLike(this.props.id)}}>
                     <div className="blue-message-body">
                         <div id={`bubble-${this.props.id}`} className="blue-bubble">
                             <p className="blue-tweet-text">{this.props.text}</p>
                         </div>
                         <div id={this.props.id} className="blue-likes">
-                            {(this.props.likes >= 1) && <img src={LikeLeft} id={`first-like-${this.props.id}`} className="first-like" style={{ marginLeft: '0', visibility: 'visible' }} alt="Like" />}
-                            {(this.props.likes >= 2) && <img src={LikeLeft} id={`second-like-${this.props.id}`} className="second-like" style={{ marginLeft: '-47px', visibility: 'visible' }} alt="Like" />}
-                            {(this.props.likes >= 3) && <img src={LikeLeft} id={`third-like-${this.props.id}`} className="third-like" style={{ marginLeft: '-47px', visibility: 'visible' }} alt="Like" />}
+                            <img src={this.props.liked? LikeLeftBlue : LikeLeft} id={`first-like-${this.props.id}`} className="first-like" style={{ marginLeft: '0', visibility: this.props.likes >= 1 ? 'visible' : 'hidden' }} alt="Like" />
+                            <img src={this.props.liked? LikeLeftBlue : LikeLeft} id={`second-like-${this.props.id}`} className="second-like" style={{ marginLeft: '-47px', visibility: this.props.likes >= 2 ? 'visible' : 'hidden' }} alt="Like" />
+                            <img src={this.props.liked? LikeLeftBlue : LikeLeft} id={`third-like-${this.props.id}`} className="third-like" style={{ marginLeft: '-47px', visibility: this.props.likes >= 3 ? 'visible' : 'hidden' }} alt="Like" />
                         </div>
                     </div>
                     <img className="blue-tail" src={TailRight} alt='Tail of blue message bubble' />
--- client/src/components/GrayMessage.js
+++ client/src/components/GrayMessage.js
@@ -7,6 +7,25 @@ import LikeRightBlue from '../assets/like_right_blue_border.png'
 let textTolink = require('text-to-link')
 
 class GrayMessage extends React.Component {
+    addLike(id) {
+        var first_like = document.getElementById(`first-like-${id}`);
+        var second_like = document.getElementById(`second-like-${id}`);
+        var third_like = document.getElementById(`third-like-${id}`);
+
+        if (third_like && third_like.style.visibility === 'visible') {
+            third_like.src = LikeRightBlue;
+        } else if (second_like && second_like.style.visibility === 'visible') {
+            third_like.src = LikeRightBlue;
+            third_like.style.visibility = 'visible';
+        } else if (first_like && first_like.style.visibility === 'visible') {
+            second_like.src = LikeRightBlue;
+            second_like.style.visibility = 'visible';
+        } else {
+            first_like.src = LikeRightBlue;
+            first_like.style.visibility = 'visible';
+        }
+    }
+
     componentDidMount() {
         var tweet_texts = document.getElementsByClassName("gray-tweet-text");
         for (let i = 0; i < tweet_texts.length; i++) {
@@ -18,18 +37,6 @@ class GrayMessage extends React.Component {
             var bubble = document.getElementById(bubble_id);
             likes[i].style.marginTop = `${-bubble.clientHeight - 25}px`;
             likes[i].style.marginLeft = `${bubble.clientWidth - 2}px`;
-
-            var first_like = document.getElementById(`first-like-${this.props.id}`);
-            var second_like = document.getElementById(`second-like-${this.props.id}`);
-            var third_like = document.getElementById(`third-like-${this.props.id}`);
-
-            if (third_like.style.visibility == 'visible') {
-
-            } else if (second_like.style.visibility == 'visible') {
-
-            } else if (first_like.style.visibility == 'visible') {
-                
-            }
         }
     }
 
@@ -37,16 +44,16 @@ class GrayMessage extends React.Component {
         return (
             <div className="gray-tweet">
                 <TweetProfilePicture name={this.props.name} url={this.props.profile_picture} diameter={40} />
-                <div className="gray-message">
+                <div className="gray-message" onDoubleClick={() => {this.addLike(this.props.id)}}>
                     <div className="gray-message-body" style={{ position: 'relative' }}>
                         <p style={{ margin: '-5px 0 -5px 27.5px', fontSize: '0.7rem', color: 'white' }}>{this.props.name}</p>
                         <div id={`bubble-${this.props.id}`} className="gray-bubble">
                             <p className="gray-tweet-text">{this.props.text}</p>
                         </div>
                         <div id={this.props.id} className="gray-likes">
-                            {(this.props.likes >= 1) && <img src={LikeRight} id={`first-like-${this.props.id}`} className="first-like" style={{ marginRight: '0', visibility: 'visible' }} alt="Like" />}
-                            {(this.props.likes >= 2) && <img src={LikeRight} id={`second-like-${this.props.id}`} className="second-like" style={{ marginRight: '-47px', visibility: 'visible' }} alt="Like" />}
-                            {(this.props.likes >= 3) && <img src={LikeRight} id={`third-like-${this.props.id}`} className="third-like" style={{ marginRight: '-47px', visibility: 'visible' }} alt="Like" />}
+                            <img src={this.props.liked? LikeRightBlue : LikeRight} id={`first-like-${this.props.id}`} className="first-like" style={{ marginRight: '0', visibility: this.props.likes >= 1 ? 'visible' : 'hidden' }} alt="Like" />
+                            <img src={this.props.liked? LikeRightBlue : LikeRight} id={`second-like-${this.props.id}`} className="second-like" style={{ marginRight: '-47px', visibility: this.props.likes >= 2 ? 'visible' : 'hidden' }} alt="Like" />
+                            <img src={this.props.liked? LikeRightBlue : LikeRight} id={`third-like-${this.props.id}`} className="third-like" style={{ marginRight: '-47px', visibility: this.props.likes >= 3 ? 'visible' : 'hidden' }} alt="Like" />
                         </div>
                     </div>
                     <img className="gray-tail" src={TailLeft} alt='Tail of gray message bubble' />
