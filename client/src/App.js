67b608f2fecefbd92db9597bd113941a20cc722a
fibanneacci <24998524+fibanneacci@users.noreply.github.com>
2021-03-29 00:44:23 -0500

    check char lim, ensure text doesn't go under button

--- client/src/App.js
+++ client/src/App.js
@@ -29,17 +29,12 @@ TODO:
 - LIKES
   - Update likes when infinite-scroll
 - MESSAGES
-  - 280-character limit
   - Exception for two adjacent Tweets from the same person (don't display profile picture, and don't put a tail)
   - Swipe left to see timestamps on messages
   - Display timestamp in middle of screen if sufficient time elapsed between two Tweets
   - Also display timestamp before the oldest Tweet of session
   - Tweeting / sending message: similar animation as when sending iMessage
-  - No reply functionality, no retweets
-    - Perhaps add retweet functionality later (retweet twice = no effect); see icons in "options-unused"
 - USERS
-  - Maybe add unfollowing functionality — "remove from groupchat" — but this might be weird because removing from groupchat is
-    two-way, unfollowing isn't
 - SCROLL
   - Infinite: display loading icons while retrieving new Tweets (upon scrolling to bottom)
   - Nonlinear - decrease speed over duration of single scroll
@@ -48,4 +43,8 @@ TODO:
   - After the "look" of the app works, connect everything to Twitter api client
     - Validate removeLike works after connecting everything - currently doesn't, because depends on actual like numbers
   - Item nesting in GrayMessage.js and BlueMessage.js and corresponding CSS in Message.css are quite messy, can be cleaned up
+  - No reply functionality, no retweets
+    - Perhaps add retweet functionality later (retweet twice = no effect); see icons in "options-unused"
+  - Maybe add unfollowing functionality — "remove from groupchat" — but this might be weird because removing from groupchat is
+    two-way, unfollowing isn't
 */
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,12 +2,23 @@ import React from 'react'
 import './Footer.css'
 import autosize from 'autosize'
 import SendTweetButton from '../assets/send.png'
+import CannotSendTweetButton from '../assets/cannot_send.png'
 
-function move_padding() {
-    var send_tweet_rect = document.getElementsByClassName("send-tweet")[0].getBoundingClientRect();
-    console.log(document.body.clientHeight);
-    console.log(send_tweet_rect.y);
+function update_send_tweet() {
+    var send_tweet = document.getElementsByClassName("send-tweet")[0];
+    var send_tweet_rect = send_tweet.getBoundingClientRect();
     document.getElementsByClassName("send-tweet-padding")[0].style.bottom = `${document.body.clientHeight - send_tweet_rect.y}px`;
+
+    var send_tweet_contents = send_tweet.value;
+    if (send_tweet_contents.length > 280) {
+        document.getElementsByClassName("send-tweet-button")[0].src = CannotSendTweetButton;
+    } else {
+        document.getElementsByClassName("send-tweet-button")[0].src = SendTweetButton;
+    }
+}
+
+function send_tweet() {
+    if (document.getElementsByClassName("send-tweet-button")[0].src === CannotSendTweetButton) return;
 }
 
 function Footer() {
@@ -19,8 +30,8 @@ function Footer() {
         <div>
             <div className="send-tweet-padding" style={{ bottom: '0' }} />
             <div className="send-tweet-container">
-                <textarea className="send-tweet" placeholder="iMessage" onChange={move_padding} rows={1}></textarea>
-                <input className="send-tweet-button" type="image" src={SendTweetButton} alt="Send tweet" />
+                <textarea className="send-tweet" onChange={update_send_tweet} placeholder="iMessage" rows={1}></textarea>
+                <input className="send-tweet-button" onClick={send_tweet} type="image" src={SendTweetButton} alt="Send tweet" />
             </div>
         </div>
     )
