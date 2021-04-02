375a8599c2daea59e4c275ecc7d357e7cb9b8169
fibanneacci <24998524+fibanneacci@users.noreply.github.com>
2021-04-02 11:53:16 -0500

    update TODO

--- client/src/App.js
+++ client/src/App.js
@@ -25,16 +25,15 @@ function App() {
 export default App
 
 /*
+CAVEATS:
+- When refreshing, recent likes go away due to lag in Twitter api
+- No reply functionality, no retweets (might accidentally subtweet)
 TODO:
-- LIKES
-  - When refreshing, recent likes go away; when reloading, all likes become blue
 - STYLE & OVERALL
-  - After the "look" of the app works, connect everything to Twitter api client (e.g. Tweeting)
-    - Validate removeLike works after connecting everything - currently doesn't, because depends on actual like numbers
+  - After the "look" of the app works, connect everything to Twitter api client (Tweeting)
 - FUTURE
   - Item nesting in GrayMessage.js and BlueMessage.js and corresponding CSS in Message.css are quite messy, can be cleaned up
-  - No reply functionality, no retweets
-    - Perhaps add retweet functionality later (retweet twice = no effect); see icons in "options-unused"
+  - Perhaps add retweet functionality later (retweet twice = no effect); see icons in "options-unused"
   - Maybe add unfollowing functionality — "remove from groupchat" — but this might be weird because removing from groupchat is
     two-way, unfollowing isn't
   - Might look better if adjacent Tweets from same user are more closely-spaced
