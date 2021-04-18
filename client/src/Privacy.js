184323131e99b57a4a3f3686c3ca15d4a9b1a76d
fibanneacci <24998524+fibanneacci@users.noreply.github.com>
2021-04-18 13:41:27 -0500

    change width

--- client/src/Privacy.js
+++ client/src/Privacy.js
@@ -4,7 +4,7 @@ import './App.css'
 function Privacy() {
     return (
         <div style={{ width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: 'black' }}>
-            <p style={{ textAlign: 'center', color: 'white' }}>
+            <p style={{ width: '500px', textAlign: 'center', color: 'white' }}>
                 This app signs you into Twitter using Twitter API, and Tweets, likes, and unlikes Tweets on your behalf. This app doesn't store your info&#8211;however, please
                 exit your browser session to fully log out.
             </p>
