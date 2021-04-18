38840fe744ca1659a6e43024c1bfce0089346fbf
fibanneacci <24998524+fibanneacci@users.noreply.github.com>
2021-04-18 15:53:36 -0500

    update privacy notice

--- client/src/Privacy.js
+++ client/src/Privacy.js
@@ -5,8 +5,8 @@ function Privacy() {
     return (
         <div style={{ width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: 'black' }}>
             <p style={{ width: '500px', textAlign: 'center', color: 'white' }}>
-                This app signs you into Twitter using Twitter API, and Tweets, likes, and unlikes Tweets on your behalf. This app doesn't store your info&#8211;however, please
-                exit your browser session to fully log out.
+                This app signs you into Twitter using Twitter API, retrieves your followers, and Tweets, likes, and unlikes Tweets on your behalf. This app doesn't store
+                your info&#8211;however, (due to a problem I'm still working on fixing!) you will need to close your browser <b>window</b> to fully log out.
             </p>
             <a className="styled-link" href="/">Back</a>
         </div>
