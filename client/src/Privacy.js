2d1e8a4a25d6cbc65cf723d2d12ae30a63a5f868
fibanneacci <24998524+fibanneacci@users.noreply.github.com>
2021-04-18 13:38:28 -0500

    font color

--- client/src/Privacy.js
+++ client/src/Privacy.js
@@ -4,8 +4,10 @@ import './App.css'
 function Privacy() {
     return (
         <div style={{ width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: 'black' }}>
-            This app signs you into Twitter using Twitter API, and Tweets, likes, and unlikes Tweets on your behalf. This app doesn't store your info&#8211;however, please
-            exit your browser session to fully log out.
+            <p style={{ textAlign: 'center', color: 'white' }}>
+                This app signs you into Twitter using Twitter API, and Tweets, likes, and unlikes Tweets on your behalf. This app doesn't store your info&#8211;however, please
+                exit your browser session to fully log out.
+            </p>
             <a className="styled-link" href="/">Back</a>
         </div>
     )
